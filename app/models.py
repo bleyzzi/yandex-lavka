5db3e668aad5c4031d3fd751eef0809358a696f4
K D <danila.krn@gmail.com>
2023-04-20 13:52:49 +0300

    add Courier class

--- app/models.py
+++ app/models.py
@@ -1,9 +1,18 @@
 from typing import List, Dict
 from fastapi import FastAPI
 from pydantic import BaseModel
+from enum import Enum
+
+
+class CourierType(Enum):
+    foot = "foot"
+    bicycle = "bicycle"
+    auto = "auto"
+
 
 class Courier(BaseModel):
     courier_id: int
-    courier_type: str
+    courier_type: CourierType
     regions: List[int]
     working_hours: List[str]
+
--- app/router.py
+++ app/router.py
@@ -1,48 +1,91 @@
 from fastapi import APIRouter, Request
 from starlette import status
+from app.models import Courier
+from typing import List, Dict
 
 router = APIRouter()
+
+
 @router.post(
     "/couriers",
     status_code=status.HTTP_200_OK
 )
-async def couriers():
+async def add_couriers_info(couriers: List[Courier]):
     """
     Обработчик принимает на вход список в формате json с данными о курьерах и графиком их работы.
     Курьеры работают только в заранее определенных районах, а также различаются по типу: пеший, велокурьер и
     курьер на автомобиле. От типа зависит объем заказов, которые перевозит курьер.
     Районы задаются целыми положительными числами. График работы задается списком строк формата `HH:MM-HH:MM`.
     """
-    pass
+
+    return {"message": "Couriers created successfully"}
+
 
 @router.get(
     "/couriers/{courier_id}",
     status_code=status.HTTP_200_OK
 )
-async def courier_info():
+async def get_single_courier_info():
     """
     Возвращает информацию о курьере
     """
     pass
 
+
 @router.get(
     "/couriers",
     status_code = status.HTTP_200_OK
 )
-async def couriers_info(offset: int = 0, limit: int = 1):
+async def get_all_couriers_info(offset: int = 0, limit: int = 1):
     """
     Возвращает информацию о всех курьерах
     Имеет поля offset и limit
     """
     pass
 
+
 @router.post(
     "/orders",
     status_code = status.HTTP_200_OK
 )
-async def orders_get():
+async def add_orders_info():
     """
     Принимает на вход список с данными о заказах в формате json. У заказа отображаются характеристики — вес, район,
     время доставки и цена.
+    """
+    pass
+
+
+@router.get(
+    "/orders/{order_id}",
+    status_code = status.HTTP_200_OK
+)
+async def get_single_order_info():
+    """
+    Возвращает информацию о заказе по его идентификатору, а также дополнительную информацию: вес заказа, район доставки,
+    промежутки времени, в которые удобно принять заказ.
+    """
+    pass
+
+
+@router.get(
+    "/orders",
+    status_code = status.HTTP_200_OK
+)
+async def get_all_orders_info(offset: int = 0, limit: int = 0):
+    """
+    Возвращает информацию о всех заказах, а также их дополнительную информацию: вес заказа, район доставки, промежутки времени, в которые удобно принять заказ.
+    Имеет поля offset и limit
+    """
+    pass
+
+
+@router.post(
+    "/orders/complete",
+    status_code = status.HTTP_200_OK
+)
+async def confirm_order():
+    """
+    Принимает массив объектов, состоящий из трех полей: id курьера, id заказа и время выполнения заказа, после отмечает, что заказ выполнен.
     """
     pass
